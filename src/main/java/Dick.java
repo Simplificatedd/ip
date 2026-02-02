84dc2e2dec0e95bcd1d6d83a92928841b000a850
Simplificatedd <simplificatedd@gmail.com>
2026-02-02 21:25:27 +0800

    Add echo feature (Level-1)

--- src/main/java/Dick.java
+++ src/main/java/Dick.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 public class Dick {
     public static void main(String[] args) {
-        String message =    "____________________________________________________________\n" +
+        System.out.println("____________________________________________________________\n" +
                             " Hello! I'm Dick\n" +
                             " What can I do for you?\n" +
-                            "____________________________________________________________\n" +
+                            "____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+
+        System.out.println("____________________________________________________________\n" +
                             " Bye. Hope to see you again soon!\n" +
-                            "____________________________________________________________\n";
-        System.out.println(message);
+                            "____________________________________________________________\n");
+
 
     }
 }
