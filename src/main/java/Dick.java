55c69df4b6b3690a5bd6439440af8f0ace3c7b4c
Simplificatedd <simplificatedd@gmail.com>
2026-02-02 21:17:22 +0800

    Rename Duke to Dick (Level-0)

---
+++ src/main/java/Dick.java
@@ -0,0 +1,12 @@
+public class Dick {
+    public static void main(String[] args) {
+        String message =    "____________________________________________________________\n" +
+                            " Hello! I'm Dick\n" +
+                            " What can I do for you?\n" +
+                            "____________________________________________________________\n" +
+                            " Bye. Hope to see you again soon!\n" +
+                            "____________________________________________________________\n";
+        System.out.println(message);
+
+    }
+}
