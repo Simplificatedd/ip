3f30a02c06bedac89cbacf4807b58e0cda184605
Simplificatedd <simplificatedd@gmail.com>
2026-03-05 03:17:55 +0800

    Add JavaDoc comments

--- src/main/java/dick/task/Todo.java
+++ src/main/java/dick/task/Todo.java
@@ -4,14 +4,19 @@ package dick.task;
  * Represents a to-do task (a task without any associated date/time).
  */
 public class Todo extends Task {
+    /**
+     * Creates a to-do task.
+     *
+     * @param description Task description.
+     */
     public Todo(String description) {
         super(description);
     }
 
     /**
-     * Creates a to-do task.
+     * Returns the task type tag used in the string representation.
      *
-     * @param description Task description.
+     * @return "[T]" for todo tasks.
      */
     @Override
     public String getType() {
